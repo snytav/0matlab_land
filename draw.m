e9c1ace9c55310e4992098385ec46ec610fff69d
Alexey Snytnikov <snytav@gmail.com>
2025-03-24 22:03:42 +0200

    Ameland as running wave pic

---
+++ draw.m
@@ -0,0 +1,26 @@
+num_years = load('num_years.txt');
+depths_uniform = load('dep.txt');
+disp(depths_uniform);
+disp(size(depths_uniform));
+NY = size(num_years);
+disp(num_years);
+disp(size(num_years));
+%surf()
+depths_uniform = reshape(depths_uniform, 34, 36);
+disp(depths_uniform);
+disp(size(depths_uniform));
+
+
+distances_uniform = load('dist_unif.txt');
+ND = size(distances_uniform);
+fig = figure;
+%ax = axes(fig, 'Projection', '3d');
+
+x = distances_uniform;
+y = num_years;
+
+%X, Y = np.meshgrid(x, y)
+z = depths_uniform;
+surf(x,y,z);
+xlabel ( '<--- X --->' )
+ylabel ( '<--- T --->' );
