ef1a9051ddfc686cbe740f47ecd05060362d82f6
Alexey Snytnikov <snytav@gmail.com>
2025-03-26 20:59:23 +0200

    surface with front line

--- draw.m
+++ draw.m
@@ -22,6 +22,19 @@ y = num_years;
 %X, Y = np.meshgrid(x, y)
 z = depths_uniform;
 surf(x,y,z);
+colorbar;
 xlabel ( '<--- X --->' )
 ylabel ( '<--- T --->' );
-saveas(gcf,'Ameland_surface.png')
+
+% Your two points
+P1 = [0,1965,-1.5];
+P2 = [320,1998,-1.5];
+
+% Their vertial concatenation is what you want
+pts = [P1; P2];
+
+% Because that's what line() wants to see    
+line(pts(:,1), pts(:,2), pts(:,3),'color','red','LineWidth',4);
+
+saveas(gcf,'Ameland_surface_front_line.png')
+qq = 0;
